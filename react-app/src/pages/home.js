bec35a478c2a3309877d45023ae3950810384a2f
Esmatullah <s3941885@student.rmit.edu.au>
2025-06-10 11:01:51 +1000

    Called calling the function to get part with for testing from home page.

--- react-app/src/pages/home.js
+++ react-app/src/pages/home.js
@@ -1,29 +1,10 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import PartSearchForm from "../components/PartSearchForm/PartSearchForm";
-import { getPartNumber } from "../data/APICalls";
 import Footer from "../components/Footer/Footer";
 import "./home.css";
 export default function Home() {
-  const [partNumber, setPartNumber] = React.useState("");
 
-  const make = "AIRMAN";
-  const model = "AX08";
-  const partType = "Rubber Track";
-  const fetchPartNumber = async (make, model, partType) => {
-    try {
-      const partData = await getPartNumber(make, model, partType);
-      setPartNumber(partData.part_number);
-    } catch (error) {
-      console.error("Error fetching part number:", error);
-    }
-  }
-
-  React.useEffect(() => {
-    fetchPartNumber(make, model, partType);
-  }, [make, model, partType]);  
-
-  
   return (
     <div className="home-page-container">
       {/* Navbar component */}
